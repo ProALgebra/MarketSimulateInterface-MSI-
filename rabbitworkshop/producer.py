487a75d1545b3be3abd812a46d207b928ffeaf10
Шишигин Степан <sshishigin190101@gmail.com>
2023-11-13 17:08:38 +0300

    dramatiq example

---
+++ rabbitworkshop/producer.py
@@ -0,0 +1,15 @@
+import dramatiq
+from dramatiq.brokers.rabbitmq import RabbitmqBroker
+
+
+rabbitmq_broker = RabbitmqBroker(host="localhost")
+dramatiq.set_broker(rabbitmq_broker)
+
+
+@dramatiq.actor()
+def work(filename, content):
+    pass
+
+if __name__ == "__main__":
+    print("запускаю таск для консумера")
+    work.send("test", "00000")
