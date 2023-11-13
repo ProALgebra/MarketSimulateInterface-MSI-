9e0727e6820b5055b95d70bc7d7d23a6bfcdb63d
MAC <90581598+sideov@users.noreply.github.com>
2023-11-13 21:43:31 +0300

    ok

--- graphics/graph.py
+++ graphics/graph.py
@@ -7,8 +7,9 @@ class GraphInterface:
         self.labels = label
 
     def plot_Total(self):
-        self.data_x = list(0, len(self.points))
-        self.data_y = self.points.values()
+        self.data_x = list(range(0, len(self.points)))
+        self.data_y = list(self.points.values())
+        print(self.data_y)
         plt.plot(self.data_x, self.data_y, label='Total')        
         plt.xlabel('Date')
         plt.ylabel('Total')
@@ -21,14 +22,14 @@ class GraphInterface:
         #plt.savefig('my_chart.png')
 
     def plot_DPNL(self):
-        self.data_x = list(0, len(self.points))
-        self.data_y = self.points.values()
+        self.data_x = list(range(0, len(self.points)))
+        self.data_y = list(self.points.values())
         plt.plot(self.data_x, self.data_y, label='DPNL', color='red')
         plt.xlabel('Date')
         plt.ylabel('DPNL')
         plt.title('DPNL Plot')
         plt.fill_between(self.data_x, self.data_y, np.zeros_like(self.data_y), color='red', alpha=0.3)
-        plt.axis([0,None,0,None])
+        # plt.axis([0,None,0,None])
         plt.grid(True, linestyle='--', alpha=0.5)
         plt.legend()
         plt.show()
@@ -57,25 +58,31 @@ class GraphInterface:
     #     plt.legend()
     #     plt.show()
       
-    # def plot_balance(self):
-    #     plt.figure(figsize=(16, 6))
-    #     plt.xlabel('Date')
-    #     plt.ylabel('balance')
-    #     plt.title('Balance Plot')
-    #     colors = ['blue', 'green', 'yellow', 'red']
-    #     plt.stackplot(rate, y, labels=['Free', 'LkoH', 'PHoS', 'SBoR'], colors=colors, alpha=0.5)
-    #     plt.legend(loc='upper left')
-    #     plt.grid(True, linestyle='--', alpha=0.5)
-    #     plt.show()
+    def plot_balance(self):
+        dates = sorted(list(self.points))
+        tikers = list(self.points[dates[0]].keys())
+        y = list(map(list, list(np.empty((len(tikers), 0)))))
+        for date in dates:
+            for i, tiker in enumerate(tikers):
+                y[i].append(self.points[date][tiker])
+        y = np.array(y, dtype=float)
+        data_x = np.array(range(0, len(y[0])), dtype=float)
+        print(len(data_x))
+        print(len(y[0]))
 
-graph_interface_1 = GraphInterface(points, label='Total')
-graph_interface_2 = GraphInterface(points, label='DPNL')
-#graph_interface_3 = GraphInterface(points, label='Sharpe')
-#graph_interface_4 = GraphInterface(points, label='relatire_Total')
-#graph_interface_5 = GraphInterface(points)
+        plt.figure(figsize=(16, 6))
+        plt.xlabel('Date')
+        plt.ylabel('balance')
+        plt.title('Balance Plot')
+        plt.stackplot(data_x, y[0], y[1], labels=tikers, alpha=0.5)
+        plt.legend(loc='upper left')
+        plt.grid(True, linestyle='--', alpha=0.5)
+        plt.show()
 
-graph_interface_1.plot_Total()
-graph_interface_2.plot_DPNL()
-#graph_interface_3.plot_sharpe()
-#graph_interface_4.plot_relatire_Total()
-#graph_interface_5.plot_balance()
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
