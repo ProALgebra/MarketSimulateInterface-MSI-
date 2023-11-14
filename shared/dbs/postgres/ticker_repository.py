f8d87b3e0c2ff375b408b0dca6c3f3c354c0504b
Шишигин Степан <sshishigin190101@gmail.com>
2023-11-14 22:58:25 +0300

    репозиторий для ticker history

---
+++ shared/dbs/postgres/ticker_repository.py
@@ -0,0 +1,63 @@
+from datetime import datetime
+
+from shared.dbs.postgres.models.ticket_history import TicketHistory
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import async_sessionmaker
+from sqlalchemy import select
+
+
+class TickerHistoryRepository:
+    def __init__(self, session: sessionmaker):
+        self.session = session
+
+    def get_tickers_in_range(self, _from: datetime, _to: datetime):
+        with self.session() as session:
+            stmt = select(TicketHistory).where((TicketHistory.day >= _from) & (TicketHistory.day < _to))
+            tickets = session.scalars(stmt)
+            return tickets.all()
+
+    def get_tickers_by_day(self, day: datetime):
+        with self.session() as session:
+            stmt = select(TicketHistory).where(TicketHistory.day == day)
+            ticket = session.scalars(stmt)
+            return ticket.all()
+
+    def insert_ticker(self, ticker: str, day: datetime, price: float):
+        with self.session() as session:
+            session.add(
+                TicketHistory(
+                    day=day,
+                    ticket=ticker,
+                    price=price
+                )
+            )
+            session.commit()
+
+
+class AsyncTickerHistoryRepository:
+    def __init__(self, session: async_sessionmaker):
+        self.session = session
+
+    async def get_tickers_in_range(self, _from: datetime, _to: datetime):
+        async with self.session() as session:
+            stmt = select(TicketHistory).where((TicketHistory.day >= _from) & (TicketHistory.day < _to))
+            tickets = await session.scalars(stmt)
+            return tickets.all()
+
+    async def get_tickers_by_day(self, day: datetime):
+        async with self.session() as session:
+            stmt = select(TicketHistory).where(TicketHistory.day == day)
+            ticket = await session.scalars(stmt)
+            return ticket.all()
+
+    async def insert_ticker(self, ticker: str, day: datetime, price: float):
+        async with self.session() as session:
+            session.add(
+                TicketHistory(
+                    day=day,
+                    ticket=ticker,
+                    price=price
+                )
+            )
+            await session.commit()
